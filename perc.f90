b3d6f461b0348263fc6caaed7697bcb2d6739724
Antoni Wellisz <awwellisz@uchicago.edu>
2023-10-29 19:25:47 -0500

    Training data

--- perc.f90
+++ perc.f90
@@ -1,7 +1,29 @@
 program perceptron
+    implicit none
+    real :: input(4, 3)  ! Training data --- 4 by 3 matrix
+    real :: output(4, 1) ! Outputs for training data 
+    real :: w(3, 1)      ! Weight vector
+    real :: alpha        ! Learning rate 
+    real :: start, end   ! Start/end times for program duration
+    integer :: i         ! Iterator variable
 
+    ! Initialize training input values
+    data input / 0, 0, 1, &
+                 1, 1, 1, & 
+                 1, 0, 1, &
+                 0, 1, 1 / 
 
+    ! Initialize training output values
+    data output / 0, 1, 1, 0 / 
 
+    ! Initialize weights with random numbers in [0, 1)
+    call random_number(w)
+
+    call cpu_time(start)
+
+    ! ... 
+
+    call cpu_time(end)
 
 end program perceptron
 
@@ -26,7 +48,7 @@ end subroutine sigmoid
 ! tanh activation function
 subroutine tanhaf(x, is_derivative)
     implicit none 
-    real, intent(inout) :: xx 
+    real, intent(inout) :: x
     logical, intent(in) :: is_derivative 
     real :: t
 
