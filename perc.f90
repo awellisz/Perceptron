f243daf278e657f4edff034bb089d9d7b46f648e
Antoni Wellisz <awwellisz@uchicago.edu>
2023-10-29 21:18:37 -0500

    Epoch vs. error rate stuff

--- perc.f90
+++ perc.f90
@@ -5,9 +5,14 @@ program perceptron
     real :: w(3, 1)       ! Weight vector
     real :: oj(4, 1)      ! Second layer of NN (output)
     real :: delta(4, 1)   ! Delta to update weights 
-    real :: alpha         ! Learning rate 
+    real :: error(4, 1)   ! Error vector
+
+    real :: alpha(4)      ! Learning rates
     real :: start, end    ! Start/end times for program duration
     integer :: i          ! Iterator variable
+
+    integer :: io         ! File input/output
+    character(32) :: filename ! Filename 
 
     ! Initialize training input values
     data oi / 0, 0, 1, &
@@ -18,29 +23,45 @@ program perceptron
     ! Initialize training output values
     data tj / 0, 1, 1, 0 / 
 
-    ! Initialize weights with random numbers in [-1, 1]
-    call random_number(w)
-    w = (2 * w) - 1
+    ! Initialize weights with random numbers in [-1, 1)
+    call random_seed() 
 
-    ! Initialize training rate
-    alpha = 1
 
-    call cpu_time(start)
+    ! Initialize training rates
+    alpha = [0.1, 1.0, 10.0, 100.0]
 
-    do i = 1, 100000
-        ! Forward propagation with sigmoid transfer function
-        oj = 1. / (1. + exp(-matmul(oi, w)))
-        ! oj = tanh(matmul(oi, w))
+    ! call cpu_time(start)
 
-        ! Find delta (amount to update weights)
-        delta = (oj - tj) * (oj * (1. - oj))
-        ! delta = (oj - toutput) * (1 - tanh(oj)**2)
+    do io = 1, 4
+        ! Construct the filename
+        write(filename, '(A,F6.1,A)') "alpha", alpha(io), ".csv"
+        open(unit=io, file=filename, status='replace', action='write')
+        ! CSV header
+        write(io,*)"epoch,error"
 
-        w = w - alpha * matmul(transpose(oi), delta)
+        ! Randomize weights
+        call random_number(w)
+        w = (2 * w) - 1
+
+        do i = 1, 100000
+            ! Forward propagation with sigmoid transfer function
+            oj = 1. / (1. + exp(-matmul(oi, w)))
+            ! oj = tanh(matmul(oi, w))
+
+            error = oj - tj
+            ! Find delta (amount to update weights)
+            delta = error * (oj * (1. - oj))
+            ! delta = error * (1 - tanh(oj)**2)
+
+            w = w - alpha(io) * matmul(transpose(oi), delta)
+            ! Calculate vector norm of error and write to file
+            write(io,*)i,",",norm2(error)
+        end do
+        close(io)
+
+        print *, oj
     end do
 
-    call cpu_time(end)
-
-    print *, oj
+    ! call cpu_time(end)
 
 end program perceptron
